6ee9a07f9a9508bcee4f02efc26decabdaf74363
leonardojgmaia <48887001+leonardojgmaia@users.noreply.github.com>
2019-04-01 20:39:24 -0300

    Create Email.java

---
+++ Email.java
@@ -0,0 +1,43 @@
+public class Email {
+
+	private int id ;
+	private String email ;
+	private String referencia ;
+
+	public int getId () {
+
+		return id ;
+
+	}
+
+	public void setId ( int i ) {
+
+		id = i ;
+	
+	}
+
+	public String getEmail () {
+
+		return email ;
+
+	}
+
+	public void setEmail ( String e ) {
+
+		email = e ;
+	
+	}
+
+	public String getReferencia () {
+
+		return referencia ;
+
+	}
+
+	public void setReferencia ( String r ) {
+
+		referencia = r ;
+	
+	}
+
+}
